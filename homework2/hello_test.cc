15e47379ebe4a7641363a3c7560e146c1578d652
AlisherZ <myzerix58@gmail.com>
2024-06-23 16:05:45 +0300

    Testing workflow for homework2

---
+++ homework2/hello_test.cc
@@ -0,0 +1,8 @@
+#include "util.h"
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  EXPECT_GT(1, 0);
+}
---
+++ homework2/util.cpp
@@ -0,0 +1,5 @@
+#include "util.h"
+
+int version() {
+	return 1;
+}
