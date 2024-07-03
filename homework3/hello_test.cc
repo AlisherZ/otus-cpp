e0fa11bc5b6175705eb4ac66194b1a8e3a943152
AlisherZ <myzerix58@gmail.com>
2024-07-04 02:52:35 +0300

    Add test for erase.

--- homework3/hello_test.cc
+++ homework3/hello_test.cc
@@ -2,6 +2,9 @@
 
 #include <gtest/gtest.h>
 
-TEST(ComparatorEqualTest, BasicEqualAssertions) {
-  EXPECT_TRUE(1 >= 0);
+TEST(MapTest, DeleteAssertions) {
+  std::map<int, int, std::less<int>, pool_allocator<std::pair<const int, int> > > m1;
+  m1[0] = 1;
+  m1.erase(0);
+  EXPECT_TRUE(m1.size() == 0);
 }
