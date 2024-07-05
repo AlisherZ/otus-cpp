3d3432655b17abb07b1cc026000caf06e78e0b52
AlisherZ <myzerix58@gmail.com>
2024-07-05 23:34:05 +0300

    Add homework4.

---
+++ homework4/print_ip.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "util.hpp"
+
+int main(int, char **)
+{
+    print_ip( int8_t{-1} );
+    print_ip( int16_t{0} );
+    print_ip( int32_t{2130706433} );
+    print_ip( int64_t{8875824491850138409} );
+    print_ip( std::string{"Hello, World!"} );
+    print_ip( std::vector<int>{100, 200, 300, 400} );
+    print_ip( std::list<short>{400, 300, 200, 100} );
+
+    return 0;
+}
---
+++ homework4/util.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <type_traits>
+#include <list>
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <class T1>
+struct is_std_vector { static const bool value=false; };
+
+template <class T1>
+struct is_std_vector<std::vector<T1> > { static const bool value=true; };
+
+template<class T1>
+constexpr bool is_std_vector_v = is_std_vector<T>::value;
+
+template <class T1>
+struct is_std_list { static const bool value=false; };
+
+template <class T1>
+struct is_std_list<std::list<T1> > { static const bool value=true; };
+
+template<class T1>
+constexpr bool is_std_list_v = is_std_list<T>::value;
+
+template <typename T>
+typename std::enable_if_t<std::is_integral_v<std::decay_t<T> > > print_ip(T ip_address)
+{
+    std::size_t num_bytes = sizeof(T);
+    constexpr uint8_t extract_byte = (uint8_t)-1;
+    std::cout << ((ip_address >> (num_bytes - 1) * 8) & extract_byte);
+    for(num_bytes--;num_bytes > 0;num_bytes--) {
+        std::cout << "." << ((ip_address >> (num_bytes - 1) * 8) & extract_byte);
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+typename std::enable_if_t<std::is_same_v<std::decay_t<T>, std::string>> print_ip(T ip_address)
+{
+    std::cout << ip_address << std::endl;
+}
+
+template <typename T>
+typename std::enable_if_t<std::disjunction_v<is_std_vector<std::decay_t<T> >, is_std_list<std::decay_t<T> > > > print_ip(T ip_address)
+{
+    bool first = true;
+    for(int ip : ip_address) {
+        if(first) {
+            first = false;
+        }
+        else {
+            std::cout << ".";
+        }
+        std::cout << ip;
+    }
+    std::cout << std::endl;
+}
