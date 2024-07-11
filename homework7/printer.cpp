0f51b328f6c9da7c39f895bc7573386ae8498033
AlisherZ <myzerix58@gmail.com>
2024-07-11 03:43:22 +0300

    Fix print empty bulk.

--- homework7/printer.cpp
+++ homework7/printer.cpp
@@ -17,10 +17,14 @@ void Printer::printToStream(CommandBulk& bulk, std::ostream& out) {
 }
 
 void ConsolePrinter::print(CommandBulk& bulk) {
-	printToStream(bulk, std::cout);
+	if(bulk.size() > 0) {
+		printToStream(bulk, std::cout);
+	}
 }
 
 void FilePrinter::print(CommandBulk& bulk) {
-	std::ofstream fout("bulk\\bulk" + std::to_string(bulk.getTimeCreated().count()) + ".txt");
-	printToStream(bulk, fout);
+	if(bulk.size() > 0) {
+		std::ofstream fout("bulk\\bulk" + std::to_string(bulk.getTimeCreated().count()) + ".txt");
+		printToStream(bulk, fout);
+	}
 }
