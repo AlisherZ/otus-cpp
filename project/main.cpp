12090071d793136bc2945dea658006b5dd75397a
AlisherZ <myzerix58@gmail.com>
2024-08-20 23:37:19 +0300

    Fix using class from proto.

--- project/main.cpp
+++ project/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
+#include <fstream>
 #include "message.pb.h"
  
 using namespace std;
  
 int main(int argc, char* argv[])
 {
-    Person john = Person.newBuilder()
-    .setId(1234)
-    .setName("John Doe")
-    .setEmail("jdoe@example.com")
-    .build();
+    Person john;
+    john.setId(1234);
+    john.setName("John Doe");
+    john.setEmail("jdoe@example.com");
     
-    output = new FileOutputStream(args[0]);
-    john.writeTo(output);
+    std::ofstream output(args[0]);
+    john.SerializeToOstream(output);
     return 0;
 }
