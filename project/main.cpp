741924d0e02874248ebd3674b55a01bcfc209c44
AlisherZ <myzerix58@gmail.com>
2024-08-22 02:07:10 +0300

    Add fftw.

--- project/main.cpp
+++ project/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <complex.h>
+#include <fftw3.h>
 #include "message.pb.h"
+#include "wavelib/header/wauxlib.h"
  
 using namespace std;
  
@@ -14,5 +17,15 @@ int main(int, char* argv[])
     std::ofstream output(argv[0]);
     john.SerializeToOstream(&output);
     output.close();
+    const int N = 10;
+    auto obj = denoise_init(N,4,"db5");
+	setDenoiseMethod(obj,"visushrink");// sureshrink is also the default. The other option with dwt and swt is visushrink.
+	// modwt works only with modwtshrink method
+	setDenoiseWTMethod(obj,"dwt");// Default is dwt. the other options are swt and modwt
+	setDenoiseWTExtension(obj,"sym");// Default is sym. the other option is per
+	setDenoiseParameters(obj,"soft","all");// Default for thresh is soft. Other option is hard
+	// Default for level is all. The other option is first
+
+	//denoise(obj,inp,oup);
     return 0;
 }
