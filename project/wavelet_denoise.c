1e4c126f6e3a34004d12f9e900b227cb68a46045
AlisherZ <myzerix58@gmail.com>
2024-08-24 02:19:51 +0300

    Trying to fix wavelet.

--- project/wavelet_denoise.c
+++ project/wavelet_denoise.c
@@ -1,8 +1,20 @@
 #include <stdlib.h>
 #include "wavelet_denoise.h"
-#include "wdenoise/header/wdenoise.h"
+//#include "wdenoise/header/wdenoise.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct wave_set{
+    int val;
+	double params[0];
+};
 
 int test() {
+    struct wave_set t;
+    t.val = 1;
+    /*
     double *sig, *inp, *oup;
 	int i, N, J;
     char *wname = "db5";
@@ -16,5 +28,10 @@ int test() {
 	inp = (double*)malloc(sizeof(double)* N);
 	oup = (double*)malloc(sizeof(double)* N);
     visushrink(inp,N,J,wname,method,ext,thresh,level,oup);
-    return 1;
+    */
+    return t.val;
 }
+
+#ifdef __cplusplus
+}
+#endif
