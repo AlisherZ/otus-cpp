81c73d03f3917f313da7304a14bc4217fa671936
AlisherZ <myzerix58@gmail.com>
2024-08-26 13:27:33 +0300

    Add output lib.

---
+++ project/output.cpp
@@ -0,0 +1,15 @@
+#include <sciplot/sciplot.hpp>
+
+#include "output.cpp"
+
+void outputInAllFormats(std::vector<double> val, std::vector<std::string> formats) {
+
+}
+
+void saveTxtFile(std::vector<double> val) {
+
+}
+
+void saveImgFile(std::vector<double> val) {
+
+}
---
+++ project/output.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+void outputInAllFormats(std::vector<double> val, std::vector<std::string> formats);
+void saveTxtFile(std::vector<double> val);
+void saveImgFile(std::vector<double> val);
