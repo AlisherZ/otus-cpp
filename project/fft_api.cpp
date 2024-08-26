b83c9342e50232df73f897b0ed8f087f1ae6f61b
AlisherZ <myzerix58@gmail.com>
2024-08-26 06:15:37 +0300

    Fix fft api.

--- project/fft_api.cpp
+++ project/fft_api.cpp
@@ -8,30 +8,30 @@ void fft_denoise_cpp(std::vector<double> inp, std::vector<double> out, FFT fft) 
     fftw_plan p = fftw_plan_dft_r2c_1d(inp.size(), inp.data(), oup, FFTW_ESTIMATE);
     fftw_execute(p);
     fftw_destroy_plan(p);
-    if(fft.get_name() == "LowPass") {
-        for(int i = fft.get_high_threshold() + 1;i < inp.size();i++) {
-            oup[i] = 0;
+    if(fft.name() == "LowPass") {
+        for(int i = fft.high_threshold() + 1;i < inp.size();i++) {
+            oup[i] = {0, 0};
         }
     }
-    if(fft.get_name() == "HighPass") {
-        for(int i = 0;i < fft.get_low_threshold();i++) {
-            oup[i] = 0;
+    if(fft.name() == "HighPass") {
+        for(int i = 0;i < fft.low_threshold();i++) {
+            oup[i] = {0, 0};
         }
     }
-    if(fft.get_name() == "BandPass") {
-        for(int i = 0;i < fft.get_low_threshold();i++) {
-            oup[i] = 0;
+    if(fft.name() == "BandPass") {
+        for(int i = 0;i < fft.low_threshold();i++) {
+            oup[i] = {0, 0};
         }
-        for(int i = fft.get_high_threshold() + 1;i < inp.size();i++) {
-            oup[i] = 0;
-        }
-    }
-    if(fft.get_name() == "BandStop") {
-        for(int i = fft.get_high_threshold() + 1;i < fft.get_low_threshold();i++) {
-            oup[i] = 0;
+        for(int i = fft.high_threshold() + 1;i < inp.size();i++) {
+            oup[i] = {0, 0};
         }
     }
-    p = fftw_plan_dft_c2r_1d(oup.size(), oup, in, FFTW_ESTIMATE);
+    if(fft.name() == "BandStop") {
+        for(int i = fft.high_threshold() + 1;i < fft.low_threshold();i++) {
+            oup[i] = {0, 0};
+        }
+    }
+    p = fftw_plan_dft_c2r_1d(inp.size(), oup, in, FFTW_ESTIMATE);
     fftw_execute(p);
     fftw_destroy_plan(p);
     for(int i = 0;i < inp.size();i++) {
