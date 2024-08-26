2fb62f7823643ea38371340a95f864fe8cd7a5ac
AlisherZ <myzerix58@gmail.com>
2024-08-26 05:49:42 +0300

    Fix api.

--- project/wavelet_cpp_api.cpp
+++ project/wavelet_cpp_api.cpp
@@ -2,6 +2,6 @@
 #include "wavelet_denoise.h"
 
 void wavelet_denoise_cpp(std::vector<double> inp, std::vector<double> out, Wavelet wt) {
-    wavelet_denoise(inp.data(), out1.data(), inp.size(), wt.levels_decomposition(), wt.name().c_str(), wt.denoising_method().c_str(), 
+    wavelet_denoise(inp.data(), out.data(), inp.size(), wt.levels_decomposition(), wt.name().c_str(), wt.denoising_method().c_str(), 
         wt.decomposition_method().c_str(), wt.signal_extension().c_str(), wt.thresholding().c_str(), wt.estimation_level().c_str());
 }
