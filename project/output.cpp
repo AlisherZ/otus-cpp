d1603e8a620fccfbfa9f39610c4a983127f7a963
AlisherZ <myzerix58@gmail.com>
2024-08-26 14:36:38 +0300

    Add output to svg.

--- project/output.cpp
+++ project/output.cpp
@@ -22,5 +22,20 @@ void saveTxtFile(std::vector<double> values) {
 }
 
 void saveImgFile(std::vector<double> values) {
-
+    Vec x = linspace(0.0, values.size(), values.size());
+    
+    Plot2D plot;
+    plot.xlabel("x");
+    plot.ylabel("y");
+    plot.xrange(0.0, values.size());
+    plot.yrange(0.0, *max_element(values.begin(), values.end()) + 1);
+    plot.legend()
+        .atOutsideBottom()
+        .displayHorizontal()
+        .displayExpandWidthBy(2);
+    plot.drawCurve(x, values[x]).label("signal");
+    Figure fig = {{plot}};
+    Canvas canvas = {{fig}};
+    //canvas.show();
+    canvas.save("result.svg");
 }
