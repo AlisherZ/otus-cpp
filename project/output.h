37fc538aae2f85f6cd184687cf61ea13f64bff80
AlisherZ <myzerix58@gmail.com>
2024-08-27 02:08:10 +0300

    Fix definition.

--- project/output.h
+++ project/output.h
@@ -4,5 +4,6 @@
 #include <vector>
 
 void outputInAllFormats(std::vector<double> values, std::vector<std::string> formats);
+void saveConsole(std::vector<double> values);
 void saveTxtFile(std::vector<double> values);
 void saveImgFile(std::vector<double> values);
