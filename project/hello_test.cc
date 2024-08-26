11ccce939db2666dde964c6e28f0af20f9a74429
AlisherZ <myzerix58@gmail.com>
2024-08-26 10:52:20 +0300

    Add fft filter test.

--- project/hello_test.cc
+++ project/hello_test.cc
@@ -27,6 +27,36 @@ TEST(FiltersTest, EmptyAssertions) {
   auto out1 = fft_denoise_cpp(inp, filt);
 
   EXPECT_EQ(out.size(), out1.size());
+  for(std::size_t i = 0;i < out.size();i++) {
+    EXPECT_DOUBLE_EQ(out[i], out1[i]);
+  }
+}
+
+TEST(FiltersTest, FFTAssertions) {
+  Filters filt;
+  std::ifstream input_config("only_fft.txt");
+  filt.ParseFromIstream(&input_config);
+  input_config.close();
+  
+  std::vector<double> inp;
+  std::string line;
+
+  std::ifstream input("test_input.txt");
+  while (std::getline(input, line)) {
+    inp.push_back(std::stod(line));
+  }
+  input.close();
+
+  std::vector<double> out;
+  std::ifstream input1("test_fft_output.txt");
+  while (std::getline(input1, line)) {
+    out.push_back(std::stod(line));
+  }
+  input1.close();
+
+  auto out1 = fft_denoise_cpp(inp, filt);
+
+  EXPECT_EQ(out.size(), out1.size());
   for(std::size_t i = 0;i < (int)out.size();i++) {
     EXPECT_DOUBLE_EQ(out[i], out1[i]);
   }
