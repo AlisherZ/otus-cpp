bbb6bb3d7a01a6ee68457c79ce71d6fa9f4e9cbd
AlisherZ <myzerix58@gmail.com>
2024-08-26 06:19:13 +0300

    Fix fft api.

--- project/fft_api.cpp
+++ project/fft_api.cpp
@@ -10,25 +10,30 @@ void fft_denoise_cpp(std::vector<double> inp, std::vector<double> out, FFT fft) 
     fftw_destroy_plan(p);
     if(fft.name() == "LowPass") {
         for(int i = fft.high_threshold() + 1;i < inp.size();i++) {
-            oup[i] = {0, 0};
+            oup[i][0] = 0;
+            oup[i][1] = 0;
         }
     }
     if(fft.name() == "HighPass") {
         for(int i = 0;i < fft.low_threshold();i++) {
-            oup[i] = {0, 0};
+            oup[i][0] = 0;
+            oup[i][1] = 0;
         }
     }
     if(fft.name() == "BandPass") {
         for(int i = 0;i < fft.low_threshold();i++) {
-            oup[i] = {0, 0};
+            oup[i][0] = 0;
+            oup[i][1] = 0;
         }
         for(int i = fft.high_threshold() + 1;i < inp.size();i++) {
-            oup[i] = {0, 0};
+            oup[i][0] = 0;
+            oup[i][1] = 0;
         }
     }
     if(fft.name() == "BandStop") {
         for(int i = fft.high_threshold() + 1;i < fft.low_threshold();i++) {
-            oup[i] = {0, 0};
+            oup[i][0] = 0;
+            oup[i][1] = 0;
         }
     }
     p = fftw_plan_dft_c2r_1d(inp.size(), oup, in, FFTW_ESTIMATE);
