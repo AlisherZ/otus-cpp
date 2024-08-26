1f41b1fa085fe37f3cab72b86b314acfa1049a92
AlisherZ <myzerix58@gmail.com>
2024-08-27 01:20:15 +0300

    test.

--- project/main.cpp
+++ project/main.cpp
@@ -40,17 +40,30 @@ int main(int argc, char* argv[])
     }
 
     Filters filt;
+    /*
     auto f = filt.add_filters();
     auto ft = f->mutable_fft();
     ft->set_name("HighPass");
     ft->set_low_threshold(4);
+    */
+    auto w = filt.add_filters();
+    auto wt = w->mutable_wt();
+    wt->set_name("db5");
+    wt->set_levels_decomposition(4);
+    wt->set_decomposition_method("dwt");
+    wt->set_denoising_method("visushrink");
+    wt->set_signal_extension("sym");
+    wt->set_thresholding("soft");
+    wt->set_estimation_level("all");
     std::string current;
     google::protobuf::util::MessageToJsonString(filt, &current);
     std::cout << current << std::endl;
 
     if (vm.count("config")) {
         std::ifstream input_config(vm["config"].as<std::string>());
-        filt.ParseFromIstream(&input_config);
+        string config;
+        input_config >> config;
+        JsonStringToMessage(config, &filt);
         input_config.close();
     }
 
