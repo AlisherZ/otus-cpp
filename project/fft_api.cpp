5d30d4432adc187b3e59c2877fc020327344b14e
AlisherZ <myzerix58@gmail.com>
2024-08-26 10:27:23 +0300

    Fix array to pointer.

--- project/fft_api.cpp
+++ project/fft_api.cpp
@@ -1,15 +1,16 @@
 #include <fftw3.h>
+#include <memory>
 
 #include "fft_api.h"
 
 void fft_denoise_cpp(std::vector<double> inp, std::vector<double> out, FFT fft) {
-    fftw_complex oup[out.size()];
-    double in[inp.size()];
-    fftw_plan p = fftw_plan_dft_r2c_1d(inp.size(), inp.data(), oup, FFTW_ESTIMATE);
+    std::shared_ptr<fftw_complex[]> oup = std::make_shared<fftw_complex[]>(out.size());
+    std::shared_ptr<double[]> in = std::make_shared<float[]>(inp.size());
+    fftw_plan p = fftw_plan_dft_r2c_1d(inp.size(), inp.data(), oup.get(), FFTW_ESTIMATE);
     fftw_execute(p);
     fftw_destroy_plan(p);
     if(fft.name() == "LowPass") {
-        for(int i = fft.high_threshold() + 1;i < inp.size();i++) {
+        for(int i = fft.high_threshold() + 1;i < (int)inp.size();i++) {
             oup[i][0] = 0;
             oup[i][1] = 0;
         }
@@ -25,7 +26,7 @@ void fft_denoise_cpp(std::vector<double> inp, std::vector<double> out, FFT fft) 
             oup[i][0] = 0;
             oup[i][1] = 0;
         }
-        for(int i = fft.high_threshold() + 1;i < inp.size();i++) {
+        for(int i = fft.high_threshold() + 1;i < (int)inp.size();i++) {
             oup[i][0] = 0;
             oup[i][1] = 0;
         }
@@ -36,7 +37,7 @@ void fft_denoise_cpp(std::vector<double> inp, std::vector<double> out, FFT fft) 
             oup[i][1] = 0;
         }
     }
-    p = fftw_plan_dft_c2r_1d(inp.size(), oup, in, FFTW_ESTIMATE);
+    p = fftw_plan_dft_c2r_1d(inp.size(), oup.get(), in.get(), FFTW_ESTIMATE);
     fftw_execute(p);
     fftw_destroy_plan(p);
     for(int i = 0;i < inp.size();i++) {
