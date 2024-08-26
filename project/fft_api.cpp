afe86260c2061e20e78732cf2d42f8e711dd99bf
AlisherZ <myzerix58@gmail.com>
2024-08-26 06:11:02 +0300

    Fix fftw3

--- project/fft_api.cpp
+++ project/fft_api.cpp
@@ -1,13 +1,12 @@
-#include "fftw.h"
+#include <fftw3.h>
+
 #include "fft_api.h"
 
 void fft_denoise_cpp(std::vector<double> inp, std::vector<double> out, FFT fft) {
-    fftw_complex in[inp.size()], oup[out.size()];
-    fftw_plan p = fftw_create_plan(inp.size(), FFTW_FORWARD, FFTW_ESTIMATE);
-    for(int i = 0;i < inp.size();i++) {
-        in[i] = inp[i];
-    }
-    fftw_one(p, in, oup);
+    fftw_complex oup[out.size()];
+    double in[inp.size()];
+    fftw_plan p = fftw_plan_dft_r2c_1d(inp.size(), inp.data(), oup, FFTW_ESTIMATE);
+    fftw_execute(p);
     fftw_destroy_plan(p);
     if(fft.get_name() == "LowPass") {
         for(int i = fft.get_high_threshold() + 1;i < inp.size();i++) {
@@ -32,10 +31,10 @@ void fft_denoise_cpp(std::vector<double> inp, std::vector<double> out, FFT fft) 
             oup[i] = 0;
         }
     }
-    p = fftw_create_plan(inp.size(), FFTW_BACKWARD, FFTW_ESTIMATE);
-    fftw_one(p, oup, in);
+    p = fftw_plan_dft_c2r_1d(oup.size(), oup, in, FFTW_ESTIMATE);
+    fftw_execute(p);
     fftw_destroy_plan(p);
     for(int i = 0;i < inp.size();i++) {
-        out[i] = in[i][0];
+        out[i] = in[i];
     }
 }
