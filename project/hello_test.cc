131a07cadb7b96a158f0406c311d351f7774bae4
AlisherZ <myzerix58@gmail.com>
2024-08-27 03:45:16 +0300

    Add test with several filters.

--- project/hello_test.cc
+++ project/hello_test.cc
@@ -96,3 +96,35 @@ TEST(FiltersTest, WTAssertions) {
     EXPECT_NEAR(out[i], out1[i], 0.1);
   }
 }
+
+TEST(FiltersTest, SeveralFiltersAssertions) {
+  Filters filt;
+  std::ifstream input_config("fft_wt.txt");
+  std::string config;
+  input_config >> config;
+  google::protobuf::util::JsonStringToMessage(config, &filt);
+  input_config.close();
+  
+  std::vector<double> inp;
+  std::string line;
+
+  std::ifstream input("test_input.txt");
+  while (std::getline(input, line)) {
+    inp.push_back(std::stod(line));
+  }
+  input.close();
+
+  std::vector<double> out;
+  std::ifstream input1("test_fft_wt_output.txt");
+  while (std::getline(input1, line)) {
+    out.push_back(std::stod(line));
+  }
+  input1.close();
+
+  auto out1 = denoise_cpp(inp, filt);
+
+  EXPECT_EQ(out.size(), out1.size());
+  for(std::size_t i = 0;i < out.size();i++) {
+    EXPECT_NEAR(out[i], out1[i], 0.1);
+  }
+}
